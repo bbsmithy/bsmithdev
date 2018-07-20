2d1e6204d3eebb19072d0d2817c55e578fc01a88
bsmith <bsmith@cartrawler.com>
2018-07-20 20:33:36 +0100

    Templating/Routing set up with pug

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -2,15 +2,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
 var browserSync = require('browser-sync').create();
 
 // Set the banner content
-var banner = ['/*!\n',
+var banner = [
+  '/*!\n',
   ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Copyright 2013-' + new Date().getFullYear(),
+  ' <%= pkg.author %>\n',
   ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
   ' */\n',
   ''
@@ -18,53 +20,59 @@ var banner = ['/*!\n',
 
 // Copy third party libraries from /node_modules into /vendor
 gulp.task('vendor', function() {
-
   // Bootstrap
-  gulp.src([
+  gulp
+    .src([
       './node_modules/bootstrap/dist/**/*',
       '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
       '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
     ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
+    .pipe(gulp.dest('./vendor/bootstrap'));
 
   // Font Awesome
-  gulp.src([
+  gulp
+    .src([
       './node_modules/font-awesome/**/*',
       '!./node_modules/font-awesome/{less,less/*}',
       '!./node_modules/font-awesome/{scss,scss/*}',
       '!./node_modules/font-awesome/.*',
       '!./node_modules/font-awesome/*.{txt,json,md}'
     ])
-    .pipe(gulp.dest('./vendor/font-awesome'))
+    .pipe(gulp.dest('./vendor/font-awesome'));
 
   // jQuery
-  gulp.src([
+  gulp
+    .src([
       './node_modules/jquery/dist/*',
       '!./node_modules/jquery/dist/core.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery'))
-
+    .pipe(gulp.dest('./vendor/jquery'));
 });
 
 // Compile SCSS
 gulp.task('css:compile', function() {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass.sync({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
+  return gulp
+    .src('./scss/**/*.scss')
+    .pipe(
+      sass
+        .sync({
+          outputStyle: 'expanded'
+        })
+        .on('error', sass.logError)
+    )
+    .pipe(gulp.dest('./css'));
 });
 
 // Minify CSS
 gulp.task('css:minify', ['css:compile'], function() {
-  return gulp.src([
-      './css/*.css',
-      '!./css/*.min.css'
-    ])
+  return gulp
+    .src(['./css/*.css', '!./css/*.min.css'])
     .pipe(cleanCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
 });
@@ -74,14 +82,14 @@ gulp.task('css', ['css:compile', 'css:minify']);
 
 // Minify JavaScript
 gulp.task('js:minify', function() {
-  return gulp.src([
-      './js/*.js',
-      '!./js/*.min.js'
-    ])
+  return gulp
+    .src(['./js/*.js', '!./js/*.min.js'])
     .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
     .pipe(gulp.dest('./js'))
     .pipe(browserSync.stream());
 });
@@ -96,7 +104,7 @@ gulp.task('default', ['css', 'js', 'vendor']);
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: './'
     }
   });
 });
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
-var app = express(),
-path = require('path'),
-publicDir = path.join(__dirname,'public');
+const pug = require('pug');
+var app = express();
+(path = require('path')), (publicDir = path.join(__dirname, 'public'));
+app.use(express.static(publicDir));
+app.set('view engine', 'pug');
 
-app.use(express.static(publicDir))
+const home = pug.renderFile('public/views/home.pug', {
+  name: 'Test'
+});
+
+app.get('/', function(req, res) {
+  res.send(home);
+});
+
+app.get('/posts', function(req, res) {
+  const post = pug.renderFile(`public/posts/${req.query.title}.pug`, {
+    title: 'test'
+  });
+  res.send(post);
+});
 
 app.listen(port);
 module.exports = app;
